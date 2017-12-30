7a5c5babc56f964f9b586bce16ad3a65b5d62e02
Jon <jondekerh@gmail.com>
2017-12-29 22:52:32 -0800

    added chat functionality in!

--- bot.js
+++ bot.js
@@ -3,6 +3,8 @@ const client = new Discord.Client();
 const auth = require('./auth.json');
 const comfyArr = require('./comfyArr.js');
 const rps = require('./minigames/rps.js');
+const chat = require('./chat.js');
+const request = require('request');
 var callOut = "c!";
 
 function autoDelete(msg) {
@@ -33,6 +35,24 @@ client.on('message', msg => {
         var p1 = argsArr[1];
         rps.rpsCompare(p1, msg);
       break;
+      //attempts to "chat" with the user using third party service:
+      case 'chat':
+        var userInput= cleanMsg.substring(7);
+        var payload = {
+          "currentNode": "",
+          "complete": null,
+          "context":{},
+          "parameters": [],
+          "extractedParameters": {},
+          "speechResponse": "",
+          "intent": {},
+          "input": '',
+          "missingParameters": []
+        };
+        payload["input"] = userInput;
+        
+        chat.chatRequest(request, payload, msg);
+     break;
       //says hi & tags user (no longer needed, feel free to replace):
       case 'test':
         msg.channel.send('hi, ' + msg.author);
